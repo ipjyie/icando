aee52e59ac002d41019116e664fb031929b49bf8
smhrd <smhrd@DESKTOP-E836UEV>
2022-09-05 10:51:52 +0900

    sql 합한 버전

--- 디지몬야구/src/model/player.java
+++ 디지몬야구/src/model/player.java
@@ -200,7 +200,7 @@ public class player {
 					break;
 				}
 			}
-		}
+		}System.out.println("경기 종료!");
 	}
 
 	public void score() {
@@ -217,10 +217,54 @@ public class player {
 		for (int i = 0; i < OpD.size(); i++) {
 			rank.add(OpD.get(i));
 		}
+		
+		int arr[] = new int[10];
+		
+		
+		
+		
 		for (int i = 0; i < rank.size(); i++) {
 
-			top = rank.get(i);
+			arr[i] = rank.get(i).score;
 		}
+		
+		for(int i=0;i<arr.length-1;i++) {
+			//현재 가장 작은 숫자의 위치 기억
+			//1회차 -0, ㅈ회차 -1, 3회차 -2
+			int index =i; //index작은숫자가 있는 자리
+			for(int j=i+1;j<arr.length;j++) {//index 번호 - index번호 바로 뒷자리 부터 비교 시작~배열긔 끝
+				//바뀔 조건
+				if(arr[index]<arr[j]) {//지금까지본 배열의 값중 가장 작은 값의 위치를 저장
+					index=j;
+				}
+				
+				}
+			//배열내에서 n번째 가장 작은 값의 위치 저장
+			//찾은 값이 원하는 위치에 저장 될 수 있게하는 코드
+			//제일 작은값 ->0 두번째로 작은값 ->1
+			if(i  != index) {//원래 숫자의 자리와 제일 작은숫자 저장된 자리가 다를때만
+			int temp= arr[index];
+			arr[index] = arr[i];
+			arr[i] =temp;
+			}
+			
+		}System.out.println(Arrays.toString(arr));
+		
+		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
